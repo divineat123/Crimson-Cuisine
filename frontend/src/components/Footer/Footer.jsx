255ff89ad9de7438948c40f6ecc91720a1bd832c
Divine Ezechukwu <divineezechu94@gmail.com>
2026-01-07 19:10:28 +0100

    Removed @

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -27,8 +27,8 @@ const Footer = () => {
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+234 9113200479</li>
-            <li>@divineezechu94@gmail.com</li>
+            <li>+234 7025086104</li>
+            <li>divineezechu94@gmail.com</li>
           </ul>
         </div>
       </div>
